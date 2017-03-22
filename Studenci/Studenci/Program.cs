a3b7191416682ed9942a0a71dbad171e5f521cb9
bartosz_szyjko <b.szyjko@hotmail.com>
2017-03-22 18:05:19 +0100

    Dodany kod.

--- Studenci/Studenci/Program.cs
+++ Studenci/Studenci/Program.cs
@@ -10,6 +10,26 @@ namespace Studenci
     {
         static void Main(string[] args)
         {
+            Student[] studenci = new Student[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Podaj nazwisko " + (i + 1) + " studenta: ");
+                studenci[i].Nazwisko = Console.ReadLine();
+                Console.Write(Environment.NewLine);
+                Console.Write("Podaj ocenę z ZIOP: ");
+                studenci[i].ZIOP = Int32.Parse(Console.ReadLine());
+                Console.Write(Environment.NewLine);
+                Console.Write("Podaj ocenę z SO: ");
+                studenci[i].SO = Int32.Parse(Console.ReadLine());
+                Console.Write(Environment.NewLine);
+                Console.Write("Podaj ocenę z Grafika: ");
+                studenci[i].Grafika = Int32.Parse(Console.ReadLine());
+                Console.Write(Environment.NewLine);
+                Console.Write("Podaj ocenę z Sql: ");
+                studenci[i].Sql = Int32.Parse(Console.ReadLine());
+                Console.Write(Environment.NewLine);
+            }
         }
     }
 }
