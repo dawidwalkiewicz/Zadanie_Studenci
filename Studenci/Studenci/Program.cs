cf2107972ea312d0b49db164a3e3ca1af020aabc
Dawid Rutkowski <dawid5180@gmail.com>
2017-03-22 18:27:53 +0100

    Ostatnie poprawki

--- Studenci/Studenci/Program.cs
+++ Studenci/Studenci/Program.cs
@@ -15,20 +15,36 @@ namespace Studenci
             for (int i = 0; i < 5; i++)
             {
                 Console.Write("Podaj nazwisko " + (i + 1) + " studenta: ");
+                studenci[i] = new Student();
                 studenci[i].Nazwisko = Console.ReadLine();
-                Console.Write(Environment.NewLine);
                 Console.Write("Podaj ocenę z ZIOP: ");
-                studenci[i].ZIOP = Int32.Parse(Console.ReadLine());
-                Console.Write(Environment.NewLine);
+                studenci[i].Oceny[0].ocena = Int32.Parse(Console.ReadLine());
                 Console.Write("Podaj ocenę z SO: ");
-                studenci[i].SO = Int32.Parse(Console.ReadLine());
-                Console.Write(Environment.NewLine);
+                studenci[i].Oceny[1].ocena = Int32.Parse(Console.ReadLine());
                 Console.Write("Podaj ocenę z Grafika: ");
-                studenci[i].Grafika = Int32.Parse(Console.ReadLine());
-                Console.Write(Environment.NewLine);
+                studenci[i].Oceny[2].ocena = Int32.Parse(Console.ReadLine());
                 Console.Write("Podaj ocenę z Sql: ");
-                studenci[i].Sql = Int32.Parse(Console.ReadLine());
+                studenci[i].Oceny[3].ocena = Int32.Parse(Console.ReadLine());
                 Console.Write(Environment.NewLine);
+            }
+
+            Console.WriteLine("Odczytywanie ocen:");
+
+            while (true)
+            {
+                Console.Write("Podaj indeks ucznia [1-5]: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                if (a == 0)
+                    break;
+
+                Student b = studenci[a - 1];
+
+                Console.Write("Podaj indeks przedmiotu [1-4]: ");
+                a = Convert.ToInt32(Console.ReadLine());
+                Ocena c = b.Oceny[a-1];
+
+                Console.WriteLine("Dla ucznia " + b.Nazwisko + " ocena z przedmiotu " + c.nazwa + " to " + c.ocena);
             }
         }
     }
@@ -36,10 +52,26 @@ namespace Studenci
     class Student
     {
         public string Nazwisko;
-        public int ZIOP, SO, Grafika, MySql;
+        public Ocena[] Oceny = new Ocena[4];
 
         public Student()
         {
+            Oceny[0] = new Ocena("ZIOP");
+            Oceny[1] = new Ocena("SO");
+            Oceny[2] = new Ocena("Grafika");
+            Oceny[3] = new Ocena("MySql");
+        }
+    }
+
+    class Ocena
+    {
+        public string nazwa;
+        public int ocena;
+
+        public Ocena(string nazwa)
+        {
+            this.nazwa = nazwa;
+            ocena = 0;
         }
     }
 }
